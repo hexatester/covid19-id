9ddb90cbfabee1a0d9310aa2393be24d09c69190
hexatester <revolusi147id@gmail.com>
2021-03-20 23:50:52 +0700

    Added DataProvinsi and get_prov

--- covid19_id/__init__.py
+++ covid19_id/__init__.py
@@ -1,9 +1,12 @@
 from .version import __version__  # NOQA
 from .update_covid19 import UpdateCovid19
-from .covid19_id import get_update
+from .provinsi import DataProvinsi
+from .covid19_id import get_update, get_prov
 
 
 __all__ = [
     "UpdateCovid19",
+    "DataProvinsi",
     "get_update",
+    "get_prov",
 ]
