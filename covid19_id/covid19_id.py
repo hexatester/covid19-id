52f5eac451f7fffe6ffbdace3b244c468f212f56
hexatester <revolusi147id@gmail.com>
2021-03-20 22:46:17 +0700

    Fix get_update

--- covid19_id/covid19_id.py
+++ covid19_id/covid19_id.py
@@ -1,12 +1,14 @@
-import attr
-from cattr import structure
+import cattr
+from datetime import date, datetime
+from dateutil.parser import parse as parse_datetime
 from urllib.request import urlopen, Request
 
 try:
     import ujson as json
 except ImportError:
-    import json
+    import json  # type: ignore[no-redef]
 
+from covid19_id.utils import ValueInt
 from . import UpdateCovid19
 from . import __version__
 
@@ -17,6 +19,9 @@ def get_update(
     data: str = ""
     headers = {"User-Agent": f"pypi.org/project/covid19-id/{__version__}"}
     req = Request(url=url, headers=headers)
+    cattr.register_structure_hook(date, lambda d, t: parse_datetime(d).date())
+    cattr.register_structure_hook(datetime, lambda d, t: parse_datetime(d))
+    cattr.register_structure_hook(ValueInt, lambda d, t: d["value"])
     with urlopen(req) as response:
         data = response.read()
-    return structure(json.loads(data), UpdateCovid19)
+    return cattr.structure(json.loads(data), UpdateCovid19)
