34d2b03f541748984663358e2478a4b79ae94999
hexatester <revolusi147id@gmail.com>
2021-03-20 21:25:51 +0700

    Add Update.today

--- covid19_id/update/update.py
+++ covid19_id/update/update.py
@@ -1,5 +1,6 @@
 import attr
-from typing import List
+from datetime import datetime
+from typing import List, Optional
 
 from . import Data
 from . import Penambahan
@@ -13,3 +14,17 @@ class Update:
     penambahan: Penambahan
     harian: List[Harian]
     total: Total
+
+    def __attrs_post_init__(self) -> None:
+        self._today: Optional[datetime] = None
+
+    @property
+    def today(self) -> Optional[Harian]:
+        if self._today:
+            return self._today
+        now = datetime.date()
+        for harian in self.harian:
+            if harian.datetime.date() == now:
+                self._today = harian
+                break
+        return self._today
