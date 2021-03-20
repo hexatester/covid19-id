f317c0f374a61ddc3e07f1f91aebafa839c307f3
hexatester <revolusi147id@gmail.com>
2021-03-20 21:13:17 +0700

    Added .get_update

--- covid19_id/__init__.py
+++ covid19_id/__init__.py
@@ -1,5 +1,6 @@
 from .version import __version__  # NOQA
 from .update import Update
+from .covid19_id import get_update
 
 
-__all__ = ["Update"]
+__all__ = ["Update", "get_update"]
---
+++ covid19_id/covid19_id.py
@@ -0,0 +1,21 @@
+from cattr import structure
+from urllib.request import urlopen, Request
+
+try:
+    import ujson as json
+except ImportError:
+    import json
+
+from . import Update
+from . import __version__
+
+
+def get_update(
+    url: str = "https://data.covid19.go.id/public/api/update.json",
+) -> Update:
+    data: str = ""
+    headers = {"User-Agent": f"pypi.org/project/covid19-id/{__version__}"}
+    req = Request(url=url, headers=headers)
+    with urlopen(req) as response:
+        data = str(response.read())
+    return structure(json.loads(data), Update)
