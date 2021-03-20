748d1f808a90b39decb91efb230fb55d234a3b86
hexatester <revolusi147id@gmail.com>
2021-03-20 22:46:48 +0700

    Add test_update_covid19

--- tests/test_covid19_id.py
+++ tests/test_covid19_id.py
@@ -1,5 +1,10 @@
-from covid19_id import __version__
+from covid19_id import __version__, get_update, UpdateCovid19
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_update_covid19():
+    update = get_update()
+    assert isinstance(update, UpdateCovid19)
