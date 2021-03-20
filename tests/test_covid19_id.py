b7802656b4f30750f8e292883a4dd8ea25dc13c0
hexatester <revolusi147id@gmail.com>
2021-03-20 23:10:45 +0700

    Bumping version from 0.1.0 to 0.2.0

--- tests/test_covid19_id.py
+++ tests/test_covid19_id.py
@@ -2,7 +2,7 @@ from covid19_id import __version__, get_update, UpdateCovid19
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.2.0"
 
 
 def test_update_covid19():
