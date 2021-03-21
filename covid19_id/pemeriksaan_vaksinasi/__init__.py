9b6edc6ede683a84781efa7edb78f42661c2fb22
hexatester <revolusi147id@gmail.com>
2021-03-21 20:55:46 +0700

    Initial Pemeriksaan

--- covid19_id/pemeriksaan_vaksinasi/__init__.py
+++ covid19_id/pemeriksaan_vaksinasi/__init__.py
@@ -1,10 +1,12 @@
 from .penambahan_pemeriksaan import PenambahanPemeriksaan
 from .pemeriksaan_harian import PemeriksaanHarian
 from .pemeriksaan_total import PemeriksaanTotal
+from .pemeriksaan import Pemeriksaan
 
 
 __all__ = [
     "PenambahanPemeriksaan",
     "PemeriksaanHarian",
     "PemeriksaanTotal",
+    "Pemeriksaan",
 ]
