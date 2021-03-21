cc1497601397d5eae0ebc3593c0d5cf555474feb
hexatester <revolusi147id@gmail.com>
2021-03-21 22:52:28 +0700

    Initial .data.perawatan.PerawatanKelompokUmur

--- covid19_id/data/perawatan/__init__.py
+++ covid19_id/data/perawatan/__init__.py
@@ -1,10 +1,12 @@
 from .base import BasePerawatan
 from .kondisi_penyerta import PerawatanKondisiPenyerta
 from .jenis_kelamin import PerawatanJenisKelamin
+from .kelompok_umur import PerawatanKelompokUmur
 
 
 __all__ = [
     "BasePerawatan",
     "PerawatanKondisiPenyerta",
     "PerawatanJenisKelamin",
+    "PerawatanKelompokUmur",
 ]
