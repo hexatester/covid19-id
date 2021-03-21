039ef7b0dfedbcd3beee56097b02be940e2de3b7
hexatester <revolusi147id@gmail.com>
2021-03-21 22:24:16 +0700

    Initial .data.kasus.JenisKelamin

--- covid19_id/data/kasus/__init__.py
+++ covid19_id/data/kasus/__init__.py
@@ -1,8 +1,10 @@
 from .base import BaseKasus
 from .kondisi_penyerta import KondisiPenyerta
+from .jenis_kelamin import JenisKelamin
 
 
 __all__ = [
     "BaseKasus",
     "KondisiPenyerta",
+    "JenisKelamin",
 ]
---
+++ covid19_id/data/kasus/jenis_kelamin.py
@@ -0,0 +1,5 @@
+from . import BaseKasus
+
+
+class JenisKelamin(BaseKasus):
+    pass
