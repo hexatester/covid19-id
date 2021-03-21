5f566c797753d54e38a410a57221d83837159916
hexatester <revolusi147id@gmail.com>
2021-03-21 22:25:14 +0700

    Initial .data.kasus.KelompokUmur

--- covid19_id/data/kasus/__init__.py
+++ covid19_id/data/kasus/__init__.py
@@ -1,10 +1,12 @@
 from .base import BaseKasus
 from .kondisi_penyerta import KondisiPenyerta
 from .jenis_kelamin import JenisKelamin
+from .kelompok_umur import KelompokUmur
 
 
 __all__ = [
     "BaseKasus",
     "KondisiPenyerta",
     "JenisKelamin",
+    "KelompokUmur",
 ]
---
+++ covid19_id/data/kasus/kelompok_umur.py
@@ -0,0 +1,5 @@
+from . import BaseKasus
+
+
+class KelompokUmur(BaseKasus):
+    pass
