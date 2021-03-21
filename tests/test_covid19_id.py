f4d52559c6acc0ae42dd1580a7561d22f8dd95c2
hexatester <revolusi147id@gmail.com>
2021-03-21 21:09:26 +0700

    Added test_pemeriksaan_vaksinasi

--- tests/test_covid19_id.py
+++ tests/test_covid19_id.py
@@ -1,4 +1,12 @@
-from covid19_id import __version__, get_update, get_prov, UpdateCovid19, DataProvinsi
+from covid19_id import (
+    __version__,
+    get_update,
+    get_prov,
+    get_pemeriksaan_vaksinasi,
+    UpdateCovid19,
+    DataProvinsi,
+    PemeriksaanVaksinasi,
+)
 
 
 def test_version():
@@ -13,3 +21,8 @@ def test_update_covid19():
 def test_get_prov():
     prov = get_prov()
     assert isinstance(prov, DataProvinsi)
+
+
+def test_pemeriksaan_vaksinasi():
+    pemeriksaan_vaksinasi = get_pemeriksaan_vaksinasi()
+    assert isinstance(pemeriksaan_vaksinasi, PemeriksaanVaksinasi)
