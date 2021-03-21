c65c8d31e95ad923e4f96781aac6b841b6f2a85e
hexatester <revolusi147id@gmail.com>
2021-03-21 22:44:08 +0700

    Initial .data.meninggal.MeninggalJenisKelamin

--- covid19_id/data/meninggal/__init__.py
+++ covid19_id/data/meninggal/__init__.py
@@ -1,8 +1,10 @@
 from .base import BaseMeninggal
 from .kondisi_penyerta import MeninggalKondisiPenyerta
+from .jenis_kelamin import MeninggalJenisKelamin
 
 
 __all__ = [
     "BaseMeninggal",
     "MeninggalKondisiPenyerta",
+    "MeninggalJenisKelamin",
 ]
