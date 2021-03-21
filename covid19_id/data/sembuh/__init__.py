718da924a68b5f5356cbbb2baeb58bf07bc0302a
hexatester <revolusi147id@gmail.com>
2021-03-21 22:36:14 +0700

    Initial .data.sembuh.SembuhKelompokUmur

--- covid19_id/data/sembuh/__init__.py
+++ covid19_id/data/sembuh/__init__.py
@@ -1,10 +1,12 @@
 from .base import BaseSembuh
 from .kondisi_penyerta import SembuhKondisiPenyerta
 from .jenis_kelamin import SembuhJenisKelamin
+from .kelompok_umur import SembuhKelompokUmur
 
 
 __all__ = [
     "BaseSembuh",
     "SembuhKondisiPenyerta",
     "SembuhJenisKelamin",
+    "SembuhKelompokUmur",
 ]
